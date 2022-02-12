12f771208e03a763a64f99dda5efd92977f38cf9
sinhadotabhinav <sinhabhinv@gmail.com>
2022-02-12 01:00:56 +0100

    used stylised components to animate

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -3,36 +3,13 @@ import PropTypes from 'prop-types';
 import GridCell from './GridCell';
 import './Grid.css';
 
-// const invalidWordAnimation = 'animate__animated animate__shakeX';
-
 export default function Grid({ letters = [] }) {
   const gridArray = Array.from(new Array(5 * 6));
   return (
     <div className="grid-container">
-      {gridArray.map((key, index) => {
-        const hasStatus = ['correct', 'missplaced', 'wrong'].includes(letters[index]?.status);
-        let bgColor = 'white';
-        let textColor = 'black';
-        if (hasStatus) {
-          if (letters[index]?.status.match('correct')) {
-            bgColor = '#538d4e';
-            textColor = 'white';
-          } else if (letters[index]?.status.match('misplaced')) {
-            bgColor = '#b49f3b';
-            textColor = 'white';
-          } else {
-            bgColor = '#aeaeae';
-            textColor = 'white';
-          }
-        }
-        const gridStyle = { backgroundColor: bgColor, color: textColor, animationDelay: hasStatus ? `${(index % 5) / 4}s` : '0s' };
-        return (
-          <GridCell
-            gridStyle={gridStyle}
-            letter={letters[index]?.letter}
-          />
-        );
-      })}
+      {gridArray.map((key, index) => (
+        <GridCell index={index} letterObject={letters[index]} />
+      ))}
     </div>
   );
 }
--- src/components/grid/GridCell.js
+++ src/components/grid/GridCell.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { keyframes } from 'styled-components';
 import './GridCell.css';
 
-export default function GridCell({ gridStyle, letter }) {
+const flip = keyframes`
+  0% {
+    transform: rotateX(0deg);
+  }
+  50% {
+    transform: rotateX(-90deg);
+  }
+  100% {
+    transform: rotateX(0deg);
+  }
+`;
+
+export default function GridCell({ index, letterObject }) {
+  const hasStatus = ['correct', 'missplaced', 'wrong']
+    .includes(letterObject?.status);
+  let bgColor = '';
+  if (hasStatus) {
+    if (letterObject?.status.match('correct')) {
+      bgColor = '#538d4e';
+    } else if (letterObject?.status.match('misplaced')) {
+      bgColor = '#b59f3a';
+    } else {
+      bgColor = '#3a3a3c';
+    }
+    const style = {
+      animation: '0.5s linear flip',
+      animationDelay: (index % 5) / 2 + 's',
+      transitionDelay: (index % 5) / 2 + 's',
+      background: bgColor,
+      color: 'white',
+    };
+    return (
+      <div className="grid-cell" style={style}>
+        {letterObject?.letter}
+      </div>
+    );
+  }
   return (
-    <div className="grid-cell" style={gridStyle}>
-      {letter}
+    <div className="grid-cell">
+      {letterObject?.letter}
     </div>
   );
 }
 
 GridCell.defaultProps = {
-  gridStyle: {},
-  letter: '',
+  index: 0,
+  letterObject: {},
 };
 
 GridCell.propTypes = {
-  gridStyle: PropTypes.instanceOf(Object),
-  letter: PropTypes.string,
+  index: PropTypes.number,
+  letterObject: PropTypes.instanceOf(Object),
 };
